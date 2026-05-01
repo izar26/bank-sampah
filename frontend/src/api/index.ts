1b248218cd173b52d6a579d9d94e0db7b49bbfab
izar <nizar.nizar.nizar.26@gmail.com>
2026-05-01 16:13:19 +0700

    build

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/api/v1";
+const API_BASE_URL = "https://api-bank-sampah.smakniscyjr.sch.id";
 
 // ── Token Management ──
 export const getAccessToken = (): string | null =>
